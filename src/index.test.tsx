4fc0d16d57948f8cc71b40279dff0e02670438dd
AZAD MOHAMED <azadhmhd@gmail.com>
2020-08-29 19:32:10 +0530

    1.0.1

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,7 @@
-import { ExampleComponent } from '.'
+import SchedulerCalendar from '.'
 
 describe('ExampleComponent', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+    expect(SchedulerCalendar).toBeTruthy()
   })
 })
