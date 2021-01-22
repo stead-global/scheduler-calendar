84598d6f727e6fa7f78eb30c549b452b986198a7
mishhub <mishhub@gmail.com>
2021-01-22 16:02:16 +0530

    Container And Interval Customisations

---
+++ example/src/App.css.js
@@ -0,0 +1,20 @@
+export default {
+    container: {
+        backgroundColor: '#ffb5b5'
+    },
+    availableContainer: {
+        color: '#5096e5',
+    },
+    emptyContainer :{
+        color: 'red'
+    },
+    sundayContainer: {
+        color: 'green'
+    },
+    disabledContainer: {
+        color: '#afb2b6'
+    },
+    intervals: {
+        color: '#5096e5'
+    },
+}
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 import SchedulerCalendar from 'scheduler-calendar'
 import 'scheduler-calendar/dist/index.css'
+import Styles from './App.css.js'
+import moment from 'moment'
 
 const App = () => {
+  const stylesOfDay = (day: string, available: boolean, _isRolling?: boolean ) => {
+    if(!_isRolling)
+      return Styles.disabledContainer
+    if(moment(day).weekday() === 0)
+      return {...Styles.container ,...Styles.sundayContainer}
+    if(available)
+      return {...Styles.container ,...Styles.availableContainer};
+    else
+      return {...Styles.container ,...Styles.emptyContainer};
+  }
   return (
     <SchedulerCalendar
       availabilities={[
@@ -26,6 +38,8 @@ const App = () => {
       availabilityType={'infinity'}
       duration={10}
       onIntervalChange={() => {}}
+      stylesOfDay={stylesOfDay}
+      intervalStyles={Styles.intervals}
     />
   )
 }
